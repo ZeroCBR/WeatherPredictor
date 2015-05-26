2a2212e4808cdf3853f6cda8c80febb8e59ff23d
DongyuZhao <dy.zhao@outlook.com>
2015-05-26 16:22:47 +1000

    Parser Done

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,24 +1,26 @@
 class Location < ActiveRecord::Base
-	has_many :measurements
-	belongs_to :postcode
-	validates :name, uniqueness: true
+  has_many :measurements
+  belongs_to :postcode
+  validates :name, uniqueness: true
 
-	def self.location_mapping(lat,long)
-		self.where(latitude: lat, longitude: long)
-	end
+  def self.location_mapping(lat, long)
+    self.where(latitude: lat, longitude: long)
+  end
 
-	def self.location_similar(lat,long)
-		locations=self.all
-		sum=1000.0
-		id=0
-		locations.each do |location|
-			if (location.latitude-lat).abs+(location.longitude-long).abs<sum
-				sum=(location.latitude-lat).abs+(location.longitude-long).abs
-				id=location.id
-			end
-		end
-		return id
-	end
+  def self.location_similar(lat, long)
+    locations = self.all
+    sum = 1296000000000
+    id = 0
+    locations.each do
+    |location|
+      distance = (location.latitude-lat)**2+(location.longitude-long)**2
+      if distance < sum
+        sum = distance
+        id=location.id
+      end
+    end
+    return id
+  end
 
 
 end
