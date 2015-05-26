b0608a726c126b6f5a27f5cb42f5110af882b07a
Zane <hellozane@163.com>
2015-05-26 14:02:01 +1000

    data controller and route.rb

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,12 +1,22 @@
 class DataController < ApplicationController
 	def data_by_pcode
-		
+		pcode = params[:postcode]
+		date = params[:date]
+		postcode = Postcode.find_by_postcode(pcode)
+		# location = Location.find_by_postcode(pcode)
+		render plain: {"date": date, "locations": postcode}.inspect
 	end
 
 	def data_by_loc
+		loc_name = params[:location_id]
+		date = params[:date]
+		location = Location.find_by_name(loc_name)
+		# location = Location.find_by_postcode(pcode)
+		render plain: {"date": date, "locations": location}.inspect
 	end
 
 	def listLocations
-		@locations=Location.all
+		@locations = Location.all
+		@postcode = Postcode.all
 	end
 end
