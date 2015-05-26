894bbf5e4062d69be05849326be7a756318be282
DongyuZhao <dy.zhao@outlook.com>
2015-05-26 18:40:51 +1000

    Parser Done

--- lib/api_key_dispatcher.rb
+++ lib/api_key_dispatcher.rb
@@ -7,17 +7,21 @@ class ApiKeyDispatcher
   end
 
   def get_current_key
+    puts("Current:#{@current_index}")
+    puts("Current:#{@key_index_map}")
+    puts("Current:#{@reversed}")
     return @key_pool[@current_index]
   end
 
   def get_a_new_key
     @current_index = @current_index + 1
-    if @reversed && @current_index >= @key_index_map
-      return nil
-    end
+
     if @current_index >= @key_pool.length
       @current_index = @current_index % @key_pool.length
       @reversed = true
+    end
+    if @reversed && @current_index >= @key_index_map
+      return nil
     end
     return get_current_key
   end
