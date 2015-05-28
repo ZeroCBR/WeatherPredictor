6a12151d8f9de6d5d799990e983bfe98db8fa1cd
sunrock <rock@ua-unistudent-ten-9-170-217.uniaccess.unimelb.edu.au>
2015-05-28 21:54:14 +1000

    all functions go well
    add link in top-bar

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -12,7 +12,6 @@ class DataController < ApplicationController
    format.html
    format.json { render json: @locations_needed }
   end
-		
 	end
 	
 	def data_by_loc
@@ -21,29 +20,22 @@ class DataController < ApplicationController
 		location = Location.find_by_name(loc_name)
 		if location != nil
 			loctionId = location.id
-			ext_by_loc = Extractor.data_by_loc_id(loctionId, date)
-			@hash_by_loc = ext_by_loc
+			@hash_by_loc = Extractor.data_by_loc_id(loctionId, date)
 		else
 			@hash_by_loc = Hash.new
 		end
-
-
 		respond_to do |format|
    format.html
    format.json { render json: @hash_by_loc }
 		end
-
 	end
 
 	def listLocations
 		@locations = Location.all
-
 		loc_to_hash = Extractor.locations_to_hash
-
 		respond_to do |format|
    format.html
    format.json { render json: loc_to_hash }
 		end
-
 	end
 end
