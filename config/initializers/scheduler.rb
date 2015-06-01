638cb1d992d4d21fd1de2972e6af424b1e6552e4
ZeroCBR <xraycbr@gmail.com>
2015-06-01 16:11:21 +1000

    schedule normal running

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,21 +1,9 @@
-# require 'rufus-scheduler'
-# require_relative '../../lib/locationLoader'
-#
-# scheduler = Rufus::Scheduler.new
-# l=LocationLoader.new
-# l.go()
-#
-# scheduler.every '1800s' do
-# 	l=LocationLoader.new
-# 	l.go()
-# end
-
 require 'rufus-scheduler'
 require_relative '../../lib/spider'
 
 scheduler = Rufus::Scheduler.new
 
-# Spider.extract
+Spider.extract
 
 scheduler.every '600s' do
 	Spider.extract
